194eedb1646f1cd4a969b1a093b0a266f33bd8aa
Alexandre José <alexandrejose2527@gmail.com>
2024-04-24 18:49:52 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+document.getElementById("open_btn").addEventListener("click", function () {
+  document.getElementById("sidebar").classList.toggle("open-sidebar");
+});
+
+//criando uma model!!
+const botaoIncluir = document.querySelector(".botao-incluir");
+const modal = document.querySelector("dialog");
+
+botaoIncluir.onclick = function () {
+  modal.showModal();
+};
+
+const fecharModal = document.querySelector(".fecharmodal");
+fecharModal.onclick = function () {
+  modal.close();
+};
+
+const inputDeOrdem = document.querySelectorAll(".input-ordem");
+const mudarIcone = document.querySelectorAll(".fa-sort-down");
+
+//ESSA FUNÇÃO AQUI EU GUARDO PRA
+inputDeOrdem.addEventListener("change", function () {
+  if (this.checked) {
+    mudarIcone.setAttribute("class", "fa-solid fa-sort-down");
+  } else {
+    mudarIcone.setAttribute("class", "fa-solid fa-sort-up");
+  }
+});
