339444ca59262f5c699479d8bddf0fc60e69e83f
Alexandre José <alexandrejose2527@gmail.com>
2024-04-25 21:54:44 -0300

    teste 678

--- js/main.js
+++ js/main.js
@@ -15,16 +15,45 @@ fecharModal.onclick = function () {
   modal.close();
 };
 
+//ESSA FUNÇÃO SERVE SO PRA MUDAR O ICONE <3 DEMOREI 75 HORAS PRA FAZER ELA
+
 const inputDeOrdem = document.querySelectorAll(".input-ordem");
+
 const mudarIcone = document.querySelectorAll(".fa-sort-down");
 
-//ESSA FUNÇÃO AQUI EU GUARDO PRA
-inputDeOrdem.addEventListener("change", function () {
-  if (this.checked) {
-    mudarIcone.setAttribute("class", "fa-solid fa-sort-down");
-  } else {
-    mudarIcone.setAttribute("class", "fa-solid fa-sort-up");
-  }
+inputDeOrdem.forEach((input) => {
+  input.addEventListener("change", function () {
+    const icone = this.closest("th").querySelector(".fa-sort-down");
+    const icone2 = this.closest("th").querySelector(".fa-sort-up");
+    if (this.checked == true) {
+      icone.setAttribute("class", "fa-solid fa-sort-up");
+      inputDeOrdem.style.display = "block";
+    } else if (this.checked == false) {
+      icone2.setAttribute("class", "fa-solid fa-sort-down");
+      inputDeOrdem.style.display = "none";
+    }
+  });
 });
 
 //teste
+var ctx = document.getElementById("myChart").getContext("2d");
+var chart = new Chart(ctx, {
+  // The type of chart we want to create
+  type: "line",
+
+  // The data for our dataset
+  data: {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "My First dataset",
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgb(255, 99, 132)",
+        data: [0, 10, 5, 2, 20, 30, 45],
+      },
+    ],
+  },
+
+  // Configuration options go here
+  options: {},
+});
